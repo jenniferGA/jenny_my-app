5a5bada477b0112824e25e88c06a0ba68cc43701
jenniferGA <nenyjegalz@gmail.com>
2021-08-05 09:44:16 -0500

    mi primer coommit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         buenas buenas
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=Gm3YR17NUY8&list=RDGm3YR17NUY8&start_radio=1"
+
           target="_blank"
           rel="noopener noreferrer"
         >
